801c0a6e8047a22b48132c33949af2ec31fd73b4
Sudhir Paturu <sudhir9p@gmail.com>
2019-05-06 07:45:31 +0530

    Basic MVC Pattrern Setup

--- src/components/news-sources/view.js
+++ src/components/news-sources/view.js
@@ -29,12 +29,22 @@ export class NewsSourcesView {
 
     updateSourcesDropdownList(data, sourcesDropdown) {
         if (data && data.length > 0) {
+            this.clearSourcesDropdownList(sourcesDropdown);
             data.forEach((item, index) => {
-                var opt = document.createElement('option');
-                opt.text = item.name;
-                opt.value = item.value;
-                sourcesDropdown.appendChild(opt);
+                const option = document.createElement('option');
+                option.text = item.name;
+                option.value = item.value;
+                sourcesDropdown.appendChild(option);
             });
+        }
+    }
+
+    clearSourcesDropdownList(sourcesDropdown) {
+        debugger;
+        if (sourcesDropdown && sourcesDropdown.options.length > 0) {
+            for (let index = 0; index < sourcesDropdown.options.length; index++) {
+                sourcesDropdown.remove(index);
+            }
         }
     }
 
--- src/shared/fetcher-proxy.js
+++ src/shared/fetcher-proxy.js
@@ -3,7 +3,12 @@ import { ApiFetcherFactory } from './apifetcher-factory';
 const target = new ApiFetcherFactory();
 
 const handler = {
-
+    // get: (target, args) => {
+    //     debugger;
+    // },
+    // fetchData:(type)=>{
+    //     debugger;
+    // }
 }
 
 export const proxy = new Proxy(target, handler);
